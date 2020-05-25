e771fb906b832f9a552958fcd756aa5ca82bd807
ln93 <376830464@qq.com>
2020-05-25 20:14:37 +0800

    rename timesetting point to st

--- es-app/src/guis/GuiMenu.cpp
+++ es-app/src/guis/GuiMenu.cpp
@@ -486,7 +486,7 @@ void GuiMenu::openTimeSettings()
 {
 	auto timesetting = std::make_shared<SliderComponent>(mWindow, 0.f, 30.f, 1.f, "m");
 	Window* window = mWindow;
-	auto s = new GuiSettings(window, "系统时间设置");
+	auto st = new GuiSettings(window, "系统时间设置");
 	time_t time_now;
 	struct tm *curr_time = NULL;
 	time(&time_now);
@@ -494,8 +494,8 @@ void GuiMenu::openTimeSettings()
 	//set year
 	auto year = std::make_shared<SliderComponent>(mWindow, 1970.f, 2099.f, 1.f, "年");
 	year->setValue((float)(curr_time->tm_year + 1900));
-	s->addWithLabel("设置年份", year);
-	s->addSaveFunc([year] {
+	st->addWithLabel("设置年份", year);
+	st->addSaveFunc([year] {
 		time_t timeset;
 		struct tm *settime = NULL;
 		time(&timeset);
@@ -508,8 +508,8 @@ void GuiMenu::openTimeSettings()
 	//set month
 	auto month = std::make_shared<SliderComponent>(mWindow, 1.f, 12.f, 1.f, "月");
 	month->setValue((float)(curr_time->tm_mon+1));
-	s->addWithLabel("设置月份", month);
-	s->addSaveFunc([month] {
+	st->addWithLabel("设置月份", month);
+	st->addSaveFunc([month] {
 		time_t timeset;
 		struct tm *settime = NULL;
 		time(&timeset);
@@ -521,8 +521,8 @@ void GuiMenu::openTimeSettings()
 	//set day
 	auto mday = std::make_shared<SliderComponent>(mWindow, 1.f, 31.f, 1.f, "日");
 	mday->setValue((float)(curr_time->tm_mday));
-	s->addWithLabel("设置日期", mday);
-	s->addSaveFunc([mday] {
+	st->addWithLabel("设置日期", mday);
+	st->addSaveFunc([mday] {
 		time_t timeset;
 		struct tm *settime = NULL;
 		time(&timeset);
@@ -535,8 +535,8 @@ void GuiMenu::openTimeSettings()
 	//set hour
 	auto hour = std::make_shared<SliderComponent>(mWindow, 0.f, 23.f, 1.f, "时");
 	hour->setValue((float)(curr_time->tm_hour));
-	s->addWithLabel("设置小时", hour);
-	s->addSaveFunc([hour] {
+	st->addWithLabel("设置小时", hour);
+	st->addSaveFunc([hour] {
 		time_t timeset;
 		struct tm *settime = NULL;
 		time(&timeset);
@@ -548,8 +548,8 @@ void GuiMenu::openTimeSettings()
 	//set min
 	auto minute = std::make_shared<SliderComponent>(mWindow, 0.f, 59.f, 1.f, "分");
 	minute->setValue((float)(curr_time->tm_min));
-	s->addWithLabel("设置分钟", minute);
-	s->addSaveFunc([minute] {
+	st->addWithLabel("设置分钟", minute);
+	st->addSaveFunc([minute] {
 		time_t timeset;
 		struct tm *settime = NULL;
 		time(&timeset);
@@ -559,7 +559,7 @@ void GuiMenu::openTimeSettings()
 		system(str.c_str());
 	});
 
-	window->pushGui(s);
+	window->pushGui(st);
 
 }
 void GuiMenu::openConfigInput()
